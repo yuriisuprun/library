4fea4f1e4c006d7c2c3ba0affeaeac10ede8bb3d
Yurii_Suprun <Yurii_Suprun@gmail.com>
2024-07-19 23:56:38 +0200

    Update BorrowRecord

--- src/main/java/com/example/library/entity/BorrowRecord.java
+++ src/main/java/com/example/library/entity/BorrowRecord.java
@@ -3,12 +3,17 @@ package com.example.library.entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Temporal;
+import jakarta.persistence.TemporalType;
+import lombok.Data;
 
 import java.time.LocalDate;
 
 /**
  * @author Yurii_Suprun
  */
+@Data
 public class BorrowRecord {
 
     @Id
@@ -16,7 +21,15 @@ public class BorrowRecord {
     private long id;
     private long userId; // foreign key to User
     private long bookId; // foreign key to Book
+    @Temporal(TemporalType.TIMESTAMP)
     private LocalDate borrowDate;
     private LocalDate dueDate;
+    @Temporal(TemporalType.TIMESTAMP)
     private LocalDate returnDate;
+
+    @ManyToOne
+    private User user;
+
+    @ManyToOne
+    private Book book;
 }
