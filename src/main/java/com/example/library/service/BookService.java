f7e07e2b6324a4e07f513db8f1e9dbc3e42615f4
Yurii_Suprun <Yurii_Suprun@gmail.com>
2024-07-19 23:10:23 +0200

    Added BookService

---
+++ src/main/java/com/example/library/service/BookService.java
@@ -0,0 +1,34 @@
+package com.example.library.service;
+
+import com.example.library.entity.Book;
+import com.example.repository.BookRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+/**
+ * @author Yurii_Suprun
+ */
+@Service
+public class BookService {
+    @Autowired
+    private BookRepository bookRepository;
+
+    public Book addBook(Book book) {
+        return bookRepository.save(book);
+    }
+
+    public Book updateBook(Book book) {
+        return bookRepository.save(book);
+    }
+
+    public void deleteBook(Long id) {
+        bookRepository.deleteById(id);
+    }
+
+    public List<Book> searchBooks(String query) {
+        return bookRepository.findByTitleContaining(query);
+    }
+}
+
