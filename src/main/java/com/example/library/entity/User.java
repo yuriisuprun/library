13af56b5b48e582ba03e624aabd611f3e69814ef
Yurii_Suprun <Yurii_Suprun@gmail.com>
2024-07-19 21:30:26 +0200

    Updated User entity

--- src/main/java/com/example/library/entity/User.java
+++ src/main/java/com/example/library/entity/User.java
@@ -1,16 +1,32 @@
 package com.example.library.entity;
 
+import jakarta.persistence.ElementCollection;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+import java.util.Set;
 
 /**
  * @author Yurii_Suprun
  */
 @Entity
+@Data
 public class User {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
     private String username;
     private String password;
-    private Role role;
+
+    @ElementCollection(fetch = FetchType.EAGER)
+    @Enumerated(EnumType.STRING)
+    private Set<Role> role;
     private String email;
 }
