5d3fac52200b770d46824f9b030ed486119a2430
Yurii_Suprun <Yurii_Suprun@gmail.com>
2024-07-19 23:02:31 +0200

    Added BorrowRecordRepository

---
+++ src/main/java/com/example/repository/BorrowRecordRepository.java
@@ -0,0 +1,14 @@
+package com.example.repository;
+
+import com.example.library.entity.BorrowRecord;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+/**
+ * @author Yurii_Suprun
+ */
+public interface BorrowRecordRepository extends JpaRepository<BorrowRecord, Long> {
+    List<BorrowRecord> findByUserId(Long userId);
+    List<BorrowRecord> findByBookId(Long bookId);
+}
